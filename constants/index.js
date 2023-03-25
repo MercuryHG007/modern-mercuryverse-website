3405e0a379cd3acbf592697b9251a32ea0f72746
Harshit Garg <ggharshit1@gmail.com>
2023-03-25 16:11:17 +0530

    Images Added and CSS Updated

--- constants/index.js
+++ constants/index.js
@@ -1,30 +1,381 @@
-export const exploreWorlds = [
+export const exploreHouse = [
   {
-    id: 'world-1',
-    imgUrl: '/planet-01.png',
-    title: 'The Hogwart',
+    id: 'house-1',
+    imgUrl: '/sigils/House_Lannister.svg',
+    title: 'House Lannister',
   },
   {
-    id: 'world-2',
-    imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    id: 'house-2',
+    imgUrl: '/sigils//House_Stark.svg',
+    title: 'House Stark',
   },
   {
-    id: 'world-3',
-    imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
+    id: 'house-3',
+    imgUrl: '/sigils//House_Targaryen.svg',
+    title: 'House Targaryen',
   },
   {
-    id: 'world-4',
-    imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    id: 'house-4',
+    imgUrl: '/sigils//House_Baratheon.svg',
+    title: 'House Baratheon',
   },
   {
-    id: 'world-5',
-    imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    id: 'house-5',
+    imgUrl: '/sigils//House_Greyjoy.svg',
+    title: 'House Greyjoy',
+  },
+  // {
+  //   id: 'house-6',
+  //   imgUrl: '/sigils//House_Tyrell.svg',
+  //   title: 'House Tyrell',
+  // },
+  {
+    id: 'house-7',
+    imgUrl: '/sigils//House_Arryn.svg',
+    title: 'House Arryn',
+  },
+  // {
+  //   id: 'house-8',
+  //   imgUrl: '/sigils//House_Tully.svg',
+  //   title: 'House Tully',
+  // },
+];
+
+export const exploreSeat = [
+  {
+    id: 'house-1',
+    imgUrl: '/seats/seat_casterlyrock.webp',
+    title: 'Casterly Rock',
+  },
+  {
+    id: 'house-2',
+    imgUrl: '/seats/seat_winterfell.webp',
+    title: 'Winterfell',
+  },
+  {
+    id: 'house-3',
+    imgUrl: '/seats/seat_dragonstone.webp',
+    title: 'Dragon Stone',
+  },
+  {
+    id: 'house-4',
+    imgUrl: '/seats/seat_kingslanding.webp',
+    title: 'Kings Landing',
+  },
+  {
+    id: 'house-5',
+    imgUrl: '/seats/seat_pyke.webp',
+    title: 'Pyke',
+  },
+  // {
+  //   id: 'house-6',
+  //   imgUrl: '/seats/seat_highgarden.webp',
+  //   title: 'Highgarden',
+  // },
+  // {
+  //   id: 'house-7',
+  //   imgUrl: '/seats/seat_eyrie.webp',
+  //   title: 'Eyrie',
+  // },
+  // {
+  //   id: 'house-8',
+  //   imgUrl: '/seats/seat_riverrun.webp',
+  //   title: 'Riverrun',
+  // },
+];
+
+const lannister = [
+  {
+    id: 1,
+    name: 'Tywin Lannister',
+    imgUrl: '/members/lannister/TywinLannister.webp',
+    desc: 'Lord Tywin Lannister was the head of House Lannister, Lord of Casterly Rock, Warden of the West, Hand of the King for three different kings, and Protector of the Realm. He was the father of Cersei, Jaime, and Tyrion Lannister, and sole grandfather of the incest-born Joffrey, Myrcella, and Tommen Baratheon.',
+  },
+  {
+    id: 2,
+    name: 'Kevan Lannister',
+    imgUrl: '/members/lannister/KevanLannister.webp',
+    desc: 'Ser Kevan Lannister was the younger brother of Lord Tywin Lannister and one of his most loyal and trusted officers in the Lannister armies during the War of the Five Kings.',
+  },
+  {
+    id: 3,
+    name: 'Cersei Lannister',
+    imgUrl: '/members/lannister/CerseiLannister.webp',
+    desc: 'Queen Cersei I Lannister was the twentieth ruler of the Seven Kingdoms and the widow of King Robert Baratheon. She was the daughter of Lord Tywin Lannister, twin sister of Jaime Lannister and elder sister of Tyrion Lannister. She was involved in an incestuous relationship with Jaime, who was secretly the father of her three bastard children, Joffrey, Myrcella and Tommen.',
+  },
+  {
+    id: 4,
+    name: 'Jaime Lannister',
+    imgUrl: '/members/lannister/JaimeLannister.webp',
+    desc: 'Ser Jaime Lannister was the elder son of Lord Tywin Lannister, younger twin brother of Queen Cersei Lannister, and older brother of Tyrion Lannister. He was involved in an incestuous relationship with Cersei, and unknown to most, he was the biological father of her three bastard children, Joffrey, Myrcella, and Tommen, as well as her unborn child.',
+  },
+  {
+    id: 5,
+    name: 'Tyrion Lannister',
+    imgUrl: '/members/lannister/TyrionLannister.webp',
+    desc: 'Lord Tyrion Lannister is the youngest child of Lord Tywin Lannister and younger brother of Cersei and Jaime Lannister. A dwarf, he uses his wit and intellect to overcome the prejudice he faces. He is the current Lord of Casterly Rock and Hand of the King to Brandon Stark.',
+  },
+  {
+    id: 6,
+    name: 'Lancel Lannister',
+    imgUrl: '/members/lannister/LancelLannister.webp',
+    desc: 'Lancel Lannister was the eldest son of Kevan Lannister and a nephew of Tywin Lannister. He served as a squire for King Robert Baratheon until Robert&#39;s death.',
   },
 ];
+
+const stark = [
+  {
+    id: 1,
+    name: 'Eddard Stark',
+    imgUrl: '/members/stark/EddardStark.webp',
+    desc: '',
+  },
+  {
+    id: 2,
+    name: 'Lyanna Stark',
+    imgUrl: '/members/stark/LyannaStark.webp',
+    desc: '',
+  },
+  {
+    id: 3,
+    name: 'Benjen Stark',
+    imgUrl: '/members/stark/BenjenStark.webp',
+    desc: '',
+  },
+  {
+    id: 4,
+    name: 'Robb Stark',
+    imgUrl: '/members/stark/RobbStark.webp',
+    desc: '',
+  },
+  {
+    id: 5,
+    name: 'Sansa Stark',
+    imgUrl: '/members/stark/SansaStark.webp',
+    desc: '',
+  },
+  {
+    id: 6,
+    name: 'Arya Stark',
+    imgUrl: '/members/stark/AryaStark.webp',
+    desc: '',
+  },
+  {
+    id: 7,
+    name: 'Bran Stark',
+    imgUrl: '/members/stark/BranStark.webp',
+    desc: '',
+  },
+  {
+    id: 8,
+    name: 'Rickon Stark',
+    imgUrl: '/members/stark/RickonStark.webp',
+    desc: '',
+  },
+  {
+    id: 9,
+    name: 'Jon Snow',
+    imgUrl: '/members/stark/JonSnow.webp',
+    desc: '',
+  },
+];
+
+const targaryen = [
+  {
+    id: 1,
+    name: 'Maestor Aemon',
+    imgUrl: '/members/targaryen/MaesterAemon.webp',
+    desc: '',
+  },
+  {
+    id: 2,
+    name: 'Aerys II Targaryen',
+    imgUrl: '/members/targaryen/AerysIITargaryen.webp',
+    desc: '',
+  },
+  {
+    id: 3,
+    name: 'Rhaegar Targaryen',
+    imgUrl: '/members/targaryen/RhaegarTargaryen.webp',
+    desc: '',
+  },
+  {
+    id: 4,
+    name: 'Viserys Targaryen',
+    imgUrl: '/members/targaryen/ViserysTargaryen.webp',
+    desc: '',
+  },
+  {
+    id: 5,
+    name: 'Daenerys Targaryen',
+    imgUrl: '/members/targaryen/DaenerysTargaryen.webp',
+    desc: '',
+  },
+];
+
+const baratheon = [
+  {
+    id: 1,
+    name: 'Robert Baratheon',
+    imgUrl: '/members/baratheon/RobertBaratheon.webp',
+    desc: '',
+  },
+  {
+    id: 2,
+    name: 'Stannis Baratheon',
+    imgUrl: '/members/baratheon/StannisBaratheon.webp',
+    desc: '',
+  },
+  {
+    id: 3,
+    name: 'Renly Baratheon',
+    imgUrl: '/members/baratheon/RenlyBaratheon.webp',
+    desc: '',
+  },
+  {
+    id: 4,
+    name: 'Gendry Baratheon',
+    imgUrl: '/members/baratheon/GendryBaratheon.webp',
+    desc: '',
+  },
+  {
+    id: 5,
+    name: 'Joffrey Baratheon',
+    imgUrl: '/members/baratheon/JoffreyBaratheon.webp',
+    desc: '',
+  },
+  {
+    id: 6,
+    name: 'Myrcella Baratheon',
+    imgUrl: '/members/baratheon/MyrcellaBaratheon.webp',
+    desc: '',
+  },
+  {
+    id: 7,
+    name: 'Tommen Baratheon',
+    imgUrl: '/members/baratheon/TommenBaratheon.webp',
+    desc: '',
+  },
+  {
+    id: 8,
+    name: 'Shireen Baratheon',
+    imgUrl: '/members/baratheon/ShireenBaratheon.webp',
+    desc: '',
+  },
+];
+
+const greyjoy = [
+  {
+    id: 1,
+    name: 'Balon Greyjoy',
+    imgUrl: '/members/greyjoy/BalonGreyjoy.webp',
+    desc: '',
+  },
+  {
+    id: 2,
+    name: 'Euron Greyjoy',
+    imgUrl: '/members/greyjoy/EuronGreyjoy.webp',
+    desc: '',
+  },
+  {
+    id: 3,
+    name: 'Yara Greyjoy',
+    imgUrl: '/members/greyjoy/YaraGreyjoy.webp',
+    desc: '',
+  },
+  {
+    id: 4,
+    name: 'Theon Greyjoy',
+    imgUrl: '/members/greyjoy/TheonGreyjoy.webp',
+    desc: '',
+  },
+];
+
+const tyrell = [
+  {
+    id: 1,
+    name: 'Olenna Tyrell',
+    imgUrl: '/members/tyrell/OlennaTyrell.webp',
+    desc: '',
+  },
+  {
+    id: 2,
+    name: 'Mace Tyrell',
+    imgUrl: '/members/tyrell/MaceTyrell.webp',
+    desc: '',
+  },
+  {
+    id: 3,
+    name: 'Margaery Tyrell',
+    imgUrl: '/members/tyrell/MargaeryTyrell.webp',
+    desc: '',
+  },
+  {
+    id: 4,
+    name: 'Loras Tyrell',
+    imgUrl: '/members/tyrell/LorasTyrell.webp',
+    desc: '',
+  },
+];
+
+const arryn = [
+  {
+    id: 1,
+    name: 'Jon Arryn',
+    imgUrl: '/members/arryn/JonArryn.webp',
+    desc: '',
+  },
+  {
+    id: 2,
+    name: 'Robbin Arryn',
+    imgUrl: '/members/arryn/RobinArryn.webp',
+    desc: '',
+  },
+];
+
+const tully = [
+  {
+    id: 1,
+    name: 'Hoster Tully',
+    imgUrl: '/members/tully/HosterTully.webp',
+    desc: '',
+  },
+  {
+    id: 1,
+    name: 'Brynden Tully',
+    imgUrl: '/members/tully/BryndenTully.webp',
+    desc: '',
+  },
+  {
+    id: 1,
+    name: 'CatelynTully',
+    imgUrl: '/members/tully/CatelynTully.webp',
+    desc: '',
+  },
+  {
+    id: 1,
+    name: 'Lysa Tully',
+    imgUrl: '/members/tully/LysaTully.webp',
+    desc: '',
+  },
+  {
+    id: 1,
+    name: 'EdmureTully',
+    imgUrl: '/members/tully/EdmureTully.webp',
+    desc: '',
+  },
+];
+
+export const houseMembers = {
+  lannister,
+  stark,
+  targaryen,
+  baratheon,
+  greyjoy,
+  tyrell,
+  arryn,
+  tully,
+};
 
 export const startingFeatures = [
   'Find a world that suits you and you want to enter',
@@ -37,13 +388,13 @@ export const newFeatures = [
     imgUrl: '/vrpano.svg',
     title: 'A new world',
     subtitle:
-        'we have the latest update with new world for you to try never mind',
+      'we have the latest update with new world for you to try never mind',
   },
   {
     imgUrl: '/headset.svg',
     title: 'More realistic',
     subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+      'In the latest update, your eyes are narrow, making the world more realistic than ever',
   },
 ];
 
@@ -52,19 +403,19 @@ export const insights = [
     imgUrl: '/planet-06.png',
     title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
     subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+      'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
     imgUrl: '/planet-07.png',
     title: '7 tips to easily master the madness of the Metaverse',
     subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+      'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
     imgUrl: '/planet-08.png',
     title: 'With one platform you can explore the whole world virtually',
     subtitle:
-        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+      'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
   },
 ];
 
